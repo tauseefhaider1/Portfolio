6a09624aed8651720c3fe89cbeaad25fd3f82346
unknown <tauseefhaider119@gmail.com>
2025-08-25 22:04:09 +0500

    Update Navbar links and App routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Herosec from './Herosec'
-import About from './About'
-import Contact from './Contact'
-import Project from './Project'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Herosec from "./Herosec";   // Home page
+import About from "./About";
+import Contact from "./Contact";
+import Project from "./Project";
+
 const App = () => {
   return (
-    <div>
+    <Router>
       <Navbar />
-      <Herosec />
-      <About />
-      <Contact />
-      <Project />
-    </div>
-  )
-}
+      <Routes>
+        <Route path="/" element={<Herosec />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Project />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,22 +1,56 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <div className="flex items-center justify-between p-2 bg-blue-700">
-        <div className="left text-white text-2xl">Portfolio</div>
-        <div className="right">
-         <ul className='flex gap-3 text-white'>
-            <li><a href="#" className='hover:text-blue-500 text-[18px]'>Home</a></li>
-            <li><a href="#" className='hover:text-blue-500 text-[18px]'>About</a></li>
-            <li><a href="#" className='hover:text-blue-500 text-[18px]'>Portfolio</a></li>
-            <li><a href="#" className='hover:text-blue-500 text-[18px]'>Contact</a></li>
-
-         </ul>
-        </div>
+    <div className="flex items-center justify-between p-2 bg-blue-700">
+      <div className="left text-white text-2xl">Portfolio</div>
+      <div className="right">
+        <ul className="flex gap-3 text-white">
+          <li>
+            <NavLink 
+              to="/" 
+              className={({ isActive }) => 
+                isActive ? "text-blue-400 font-bold text-[18px]" : "hover:text-blue-400 text-[18px]"
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink 
+              to="/about" 
+              className={({ isActive }) => 
+                isActive ? "text-blue-400 font-bold text-[18px]" : "hover:text-blue-400 text-[18px]"
+              }
+            >
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink 
+              to="/projects" 
+              className={({ isActive }) => 
+                isActive ? "text-blue-400 font-bold text-[18px]" : "hover:text-blue-400 text-[18px]"
+              }
+            >
+              Portfolio
+            </NavLink>
+          </li>
+          <li>
+            <NavLink 
+              to="/contact" 
+              className={({ isActive }) => 
+                isActive ? "text-blue-400 font-bold text-[18px]" : "hover:text-blue-400 text-[18px]"
+              }
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
